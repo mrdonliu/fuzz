e6c5625f16266c7d8050a5b0922073c53ce9b3c5
unknown <mrdonliu@gmail.com>
2020-09-29 20:32:45 -0400

    Changed getline

--- fuzz.c
+++ fuzz.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 void startShell();
 int main(){
     startShell();
@@ -8,10 +11,23 @@ int main(){
 
 
 void startShell(){
+    char * command = NULL;
+    size_t len = 0;
     while(1){
+        if (getline(&command, &len, stdin) == -1){
+            perror("getline");
+            if( feof(stdin) ){
+                exit(0);
+            } else {
+                exit(1);
+            }
+        }
         
-        char* line = readline(">");
-        printf(line);
+
+        pid_t pid = fork();
+        if( pid == 0 ){
+
+        }
     }
     
 
